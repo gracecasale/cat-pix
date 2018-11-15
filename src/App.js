ae14d14e0fbee9bc0be4e21a8d7ec41dd9282b9c
Grace Casale <gracecasale@gmail.com>
2018-11-15 18:52:56 -0500

    added CSS Cat image, imported axios and modal, and began working on the modal opening.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,77 @@
 import React, { Component } from 'react';
+import Modal from "react-modal";
+import Axios from 'axios';
 import './App.css';
 
+const appElement = document.getElementById('root');
+Modal.setAppElement(appElement);
+
+const CATPIX_API = isDev ? '/activity' : 'https://aws.random.cat/meow';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false,
+      error: null,
+    }
+    this.handleModalOpen = this.handleModalOpen.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
+  }
+  handleModalOpen() {
+    axios.get(CATPIX_API)
+      .then(response => {
+        const { activty } = response.data;
+        const { participants } = response.data;
+        this.setState({
+          modalOpen: true,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err
+        });
+      })
+  }
+  handleModalClose() {
+    this.setState({ modalOpen: false });
+  }
+
   render() {
     return (
-      <div className="App">
-       It works!
-      </div>
+      <div>
+        <header className="App-header">
+          Cat Pix App
+       </header>
+        <main className="App-main">
+          <div class="wrapper">
+
+            <div class="cat">
+              <span class="eyes left"></span>
+              <span class="eyes right"></span>
+              <span class="mouth"></span>
+            </div>
+
+            <div class="moon">
+              <div class="sphere"></div>
+
+            </div>
+
+            <div class="cloud-container">
+              <div class="cloud"></div>
+              <div class="cloud"></div>
+              <div class="cloud"></div>
+              <div class="cloud"></div>
+            </div>
+
+          </div>
+
+        </main>
+        <footer className="App-footer">
+
+        </footer>
+      </div >
     );
   }
 }
